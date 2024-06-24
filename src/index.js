d8457b5273fa19e53ceea60b47e614951ab667f7
warlocks365 <eudemon365@gmail.com>
2024-06-24 14:12:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.bathingmoonlight.cn": dockerHub,
+  "quay.bathingmoonlight.cn": "https://quay.io",
+  "gcr.bathingmoonlight.cn": "https://gcr.io",
+  "k8s-gcr.bathingmoonlight.cn": "https://k8s.gcr.io",
+  "k8s.bathingmoonlight.cn": "https://registry.k8s.io",
+  "ghcr.bathingmoonlight.cn": "https://ghcr.io",
+  "cloudsmith.bathingmoonlight.cn": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.bathingmoonlight.cn": dockerHub,
 };
 
 function routeByHosts(host) {
